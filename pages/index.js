91a69569c8b9779fc8051bccf0d409a0d9496c46
vivek basant <vivekbasant50@gmail.com>
2020-12-01 20:00:43 +0530

    styles added

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,16 @@ import Head from "next/head";
 import {getPostsData} from "../components/api"
 import Gallery from "../components/Gallery"
 import "react-photoswipe/lib/photoswipe.css";
+import styles from "../styles/Home.module.css";
 
 export default function Home(props) {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
-        <title> NextJs Image Gallery</title>
+        <title > NextJs Image Gallery</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>NextJS Image Gallery</h1>
+      <h1 className={styles.title}>NextJS Image Gallery</h1>
       <Gallery pic = {props.item} />
       
     </div>
